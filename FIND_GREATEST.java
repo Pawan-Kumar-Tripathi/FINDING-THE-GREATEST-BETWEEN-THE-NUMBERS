eeba84cfe7fd9f907feecc83c07b09586d2877ff
PAWAN KUMAR TRIPATHI <78968923+Pawan-Kumar-Tripathi@users.noreply.github.com>
2022-06-25 21:00:11 +0530

    Add files via upload

---
+++ FIND_GREATEST.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+class FIND_GREATEST
+    {
+        public void main()
+        {
+            Scanner sc = new Scanner(System.in);
+            int a = sc.nextInt();
+            int b= sc.nextInt();
+            int c = sc.nextInt();
+            
+            if(a>b && a>c)
+            {
+                System.out.println("A is greater");
+            }
+            else if(b>a && b>c)
+            {
+                System.out.println("B is greater");
+            }
+            else
+            {
+                System.out.println("C is greater");
+            }
+        }
+    }
